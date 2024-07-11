1c63f19f02c15c2b5567c55602b3bfa6bd31dafb
Niko Maresco P52 <niko.maresco@cloud.com>
2024-07-11 16:49:52 -0400

    initial commit

---
+++ src/scripts/wafer-mapchart.ts
@@ -0,0 +1,186 @@
+const { ConnectivityService } = Spotfire.Dxp.Framework.ApplicationModel;
+const { MarkerClass, CategoryKey } = Spotfire.Dxp.Application.Visuals;
+const { MapChart, MarkerLayerVisualization, Projection } = Spotfire.Dxp.Application.Visuals.Maps;
+const { LibraryManager, LibraryItem, LibraryItemType } = Spotfire.Dxp.Framework.Library;
+
+
+// markings with these names will be created if they don't already exist
+const MARKING1_NAME = "Marking";
+const MARKING2_NAME = "Marking (2)";
+
+// default values for the wafer map chart
+
+const WAFER_PK_COL_NAME = "New Wafer";
+
+const DIE_X_COL_NAME = "Die X";
+const DIE_Y_COL_NAME = "Die Y";
+
+const COLOR_AXIS_COL_NAME = "Bin";
+
+const MAP_TRELLIS_ROWS = 3;
+const MAP_TRELLIS_COLS = 7;
+const MAP_TITLE = "Wafer bin map";
+const MARKER_LAYER_TITLE = "die layer";
+
+const COLORSCHEME_LIBRARY_PATH = "/bigwafer_colors";
+
+
+/*
+* attempts to verify that the Spotfire Analyst client is connected to a Spotfire Server
+*
+* @param application - the current application context
+* @returns true if the client is connected to a server, false otherwise
+*/
+function checkServerConnection(application: Spotfire.Dxp.Application.AnalysisApplication): boolean {
+    const connectivityService = application.GetService(ConnectivityService);
+    if (connectivityService == null || !connectivityService)
+        return false;
+    return connectivityService.IsOnline;
+}
+
+/*
+* attempts to get a LibraryItem with the given path and type from the Spotfire,
+* Library. throws an error if the item is not found.
+*
+* @param lm - the LibraryManager to use to look up the item. get with `application.GetService(LibraryManager)`
+* @param itemPath - the path to the item in the Library
+* @param itemType - the type of the item to look up
+* @returns the LibraryItem with the given path and type
+*/
+function getLibraryItem(
+    lm: Spotfire.Dxp.Framework.Library.LibraryManager,
+    itemPath: string,
+    itemType: Spotfire.Dxp.Framework.Library.LibraryItemType
+): Spotfire.Dxp.Framework.Library.LibraryItem {
+    const item = OutParam.create(LibraryItem);
+    if (!lm.TryGetItem(itemPath, itemType, item.out)) {
+        throw new Error(`Library item not found: ${itemPath}`);
+    }
+    return item;
+}
+/*
+* evaluates if a Marking with the given name exists in the document
+*
+* @param document - the current Document context
+* @param markingName - the name of the Marking to check for
+* @returns true if the Marking exists, false otherwise
+*/
+function markingExists(
+    document: Spotfire.Dxp.Application.Document,
+    markingName: string
+): boolean {
+    return document.Data.Markings.Contains(markingName);
+}
+
+/*
+* gets a Marking with the given name, creating it if it doesn't exist
+*
+* optionally accepts a `markingColor` to set the color of a created Marking. if
+*  the Marking already exists, the color will not be changed. if the Marking
+*  doesn't exist, and `markingColor` is not specified, the color will be
+*  managed by Spotfire.
+*
+* @param document - the current Document context
+* @param markingName - the name of the Marking to get or create
+* @param markingColor - optional. the color to set for the Marking
+* @returns the Marking with the given name
+*/
+function getOrCreateMarking(
+    document: Spotfire.Dxp.Application.Document,
+    markingName: string,
+    markingColor?: System.Drawing.Color
+): Spotfire.Dxp.Data.DataMarkingSelection {
+    if (markingExists(document, markingName)) {
+        return document.Data.Markings.Item.get(markingName)!;
+    }
+    const marking = document.Data.Markings.Add(markingName);
+    if (markingColor !== undefined) {
+        marking.Color = markingColor;
+    }
+    return marking;
+}
+
+export function createMapchart({
+    document,
+    application,
+}: CreateWaferMapchartParameters) {
+
+    // get the current page, or create a new one if one doesn't exist
+    const page = document.ActivePageReference ?? document.Pages.AddNew();
+    // get the current data table, or the first one added to the document if one is not active
+    const dataTable = document.ActiveDataTableReference ?? Array.from(document.Data.Tables)[0];
+
+    const marking1 = getOrCreateMarking(document, MARKING1_NAME);
+    const marking2 = getOrCreateMarking(document, MARKING2_NAME);
+
+    // create a new MapChart
+    const mapChart = page.Visuals.AddNew(MapChart);
+    mapChart.Layers.Clear();
+
+    // set up basic map chart properties
+    mapChart.Title = MAP_TITLE;
+    mapChart.Projection = Projection.None;
+    //mapChart.Legend.Visible = false;
+
+    // create and configure the marking layer
+    const markerLayer = OutParam.create(MarkerLayerVisualization);
+    mapChart.Layers.AddNewMarkerLayer(dataTable, markerLayer.out);
+    markerLayer.AutoConfigure();
+
+    // basic layer properties
+    markerLayer.Title = MARKER_LAYER_TITLE;
+    markerLayer.MarkerClass = MarkerClass.Tile;
+    //markerLayer.MarkerByAxis.Expression = `<[${DIE_X_COL_NAME}] NEST [${DIE_Y_COL_NAME}]>`;
+
+    // for whatever reason, the Projection property doesn't bubble up from
+    //  MapChartLayer to MapChartDataLayer/MarkerLayerVisualization, so we have
+    //  to get the layer as a regular Layer and set the Projection property there
+    const markerLayerAsRegularLayer = mapChart.Layers.Item.get(0)!;
+    markerLayerAsRegularLayer.Projection = Projection.None;
+
+    markerLayer.XAxis.Expression=`[${DIE_X_COL_NAME}]`;
+    markerLayer.YAxis.Expression=`[${DIE_Y_COL_NAME}]`;
+
+    // set the color axis and set first Bin color to a light gray
+    // (keeping this as a fallback in case the library is unavailable)
+    markerLayer.ColorAxis.Expression = `<${COLOR_AXIS_COL_NAME}>`;
+    const lightGray = System.Drawing.Color.FromArgb(255, 241, 241, 241);
+    markerLayer.ColorAxis.Coloring.SetColorForCategory(new CategoryKey(1), lightGray);
+
+    // set up Mmarking behavior
+    // markerLayer.Data.MarkingReference = marking2;
+    // markerLayer.Data.Filterings.Add(marking1);
+    // markerLayer.Data.Filterings.Add(marking2);
+    // markerLayer.Data.MarkingCombinationMethod = DataSelectionCombinationMethod.Union;
+    // markerLayer.Data.LimitingMarkingsEmptyBehavior = LimitingMarkingsEmptyBehavior.ShowEmpty;
+
+    // this must be set after the markerLayer is created
+    // configure trellis properties
+    mapChart.Trellis.PanelAxis.Expression = `<[${WAFER_PK_COL_NAME}]>`;
+    mapChart.Trellis.ManualLayout = true;
+    mapChart.Trellis.ManualRowCount = MAP_TRELLIS_ROWS;
+    mapChart.Trellis.ManualColumnCount = MAP_TRELLIS_COLS;
+
+    // only proceed if we are connected to a server
+    if (checkServerConnection(application)) {
+        const libraryManager = application.GetService(LibraryManager);
+        if (libraryManager == null || !libraryManager) {
+            throw new Error("LibraryManager service not available");
+        }
+
+        // look up the color scheme at `/bigwafer_colors` in the Spotfire Library
+        const libraryColorScheme = getLibraryItem(
+            libraryManager,
+            COLORSCHEME_LIBRARY_PATH,
+            LibraryItemType.ColorScheme
+        );
+
+        // add the scheme to the local document
+        const documentColorScheme = document.ColoringTemplates.AddFromLibrary(libraryColorScheme);
+
+        // apply the scheme to the marker layer
+        markerLayer.ColorAxis.Coloring.Apply(documentColorScheme.DisplayName);
+    }
+}
+
+RegisterEntryPoint(createMapchart);
---
+++ src/scripts/wafer-transform.ts
@@ -0,0 +1,358 @@
+const { DataTable, DataColumn, DataColumnSignature, AddColumnsSettings, JoinType,
+    DataFlowBuilder, DataSourcePromptMode, DataType, DataRowReaderColumn } = Spotfire.Dxp.Data;
+const { PivotTransformation, UnpivotTransformation, ReplaceValuesTransformation,
+        AddCalculatedColumnTransformation, ColumnAggregation, ReplaceColumnTransformation } = Spotfire.Dxp.Data.Transformations;
+const { DataTableDataSource, ImportContext } = Spotfire.Dxp.Data.Import;
+const { Dictionary, List } = System.Collections.Generic;
+
+const STEP1_RESULTNAMINGEXPRESSION = "%V.%C";
+const STEP5_RESULTNAMINGEXPRESSION = "%C";
+
+// default values for testing
+const D_PRIMARYKEYCOLUMNS = "New Wafer,Bin";
+const D_TARGETCOLUMNTITLES = "Circle,Segment,Mask";
+const D_TARGETCOLUMNVALUES = "CirclePct,SegmentPct,MaskPct";
+const D_TARGETMEASURE = "Avg";
+const D_OUTPUTDATATABLENAME = "WaferTransform";
+
+/*
+* tries to get a column by name from a data table, throws an error if it doesn't exist
+*
+* @param dataTable - the data table to search
+* @param columnName - the name of the column to find
+* @returns the column if it exists
+*/
+function getColumn(
+    dataTable: Spotfire.Dxp.Data.DataTable,
+    columnName: string
+): Spotfire.Dxp.Data.DataColumn {
+    const col = OutParam.create(DataColumn);
+    if (!dataTable.Columns.TryGetValue(columnName, col.out))
+        throw new Error(`Cannot find column '${columnName}' in table '${dataTable.Name}'.`);
+    return col;
+}
+
+/*
+* zips two arrays together
+*
+* @param arr1 - the first array
+* @param arr2 - the second array
+* @returns an array of arrays where each sub-array contains the corresponding elements from the input arrays
+*/
+function zip(arr1: any[], arr2: any[]): any[] {
+    return arr1.map((k, i) => [k, arr2[i]]);
+}
+
+/*
+* converts a csv string of column names to an array of DataColumns
+*
+* @param dataTable - the data table to search
+* @param columnNameCsv - a csv string of column names to find
+* @returns an array of columns
+*/
+function getColumnsFromCsv(
+    dataTable: Spotfire.Dxp.Data.DataTable,
+    columnNameCsv: string
+): Spotfire.Dxp.Data.DataColumn[] {
+    const columnNames = columnNameCsv.split(",");
+    const results = columnNames.map(columnName => getColumn(dataTable, columnName));
+    return results;
+}
+
+/*
+* converts an array of DataColumns to an array of DataColumnSignatures
+*
+* to convert a single DataColumn, use `new DataColumnSignature(dataColumn)`
+*
+* @param columns - an array of DataColumns to convert
+* @returns an array of DataColumnSignatures
+*/
+function convertDataColumnsToDataSignatures(
+    columns: Spotfire.Dxp.Data.DataColumn[]
+): Spotfire.Dxp.Data.DataColumnSignature[] {
+    return columns.map(col => new DataColumnSignature(col));
+}
+
+/*
+* creates a new DataTable or replaces an existing one with a "fresh" copy of sourceTable
+*
+* @param document - Spotfire Document context
+* @param tableName - the name of the table to create
+* @param sourceTable - the DataTable to copy from
+* @returns the new table
+*/
+function createOrReplaceDataTable(
+    document: Spotfire.Dxp.Application.Document,
+    tableName: string,
+    sourceTable: Spotfire.Dxp.Data.DataSource
+): Spotfire.Dxp.Data.DataTable {
+    if (document.Data.Tables.Contains(tableName)) {
+        // safe to use ! here since we know the table exists
+        const existingTable = document.Data.Tables.Item.get(tableName)!;
+        document.Data.Tables.Remove(existingTable);
+    }
+    const newTable = document.Data.Tables.Add(tableName, sourceTable);
+    return newTable;
+}
+
+function validateInputs(): boolean {
+    // make sure that both column lists contain the same number of values
+    return true;
+}
+
+/*
+* applies a series of transformations to a DataTable
+*
+* @param importContext - the ImportContext to use. this is typically `application.ImportContext`
+* @param sourceDataTable - the DataTable to use as a source for the transformations
+* @param pkColumns - an array of DataColumns to use as the primary key
+* @param titleColumns - an array of DataColumns to use as category columns
+* @param valueColumns - an array of DataColumns to aggregate
+* @param targetMeasure - the aggregation method to use
+* @returns void
+*/
+function applyTransformSeries(
+    importContext: Spotfire.Dxp.Data.Import.ImportContext,
+    dataSource: Spotfire.Dxp.Data.DataSource,
+    pkColumns: Spotfire.Dxp.Data.DataColumn[],
+    titleColumns: Spotfire.Dxp.Data.DataColumn[],
+    valueColumns: Spotfire.Dxp.Data.DataColumn[],
+    targetMeasure: string,
+): Spotfire.Dxp.Data.DataSource {
+
+    // since we are creating a "virtual" instance of baseDataTable for each iteration, we need to
+    //  use a DataFlowBuilder to build the transformation series and apply it to a DataSource,
+    //  which will ultimately be used in the Add Columns operation
+    const dfb = new DataFlowBuilder(dataSource, importContext);
+
+//1: pivot
+    // the first Pivot has to be applied immediately or we won't know which columns
+    //  to use during the Unpivot step
+
+    const identityColumnSignatures = convertDataColumnsToDataSignatures(pkColumns);
+    const categoryColumnSignatures = convertDataColumnsToDataSignatures(titleColumns);
+    const valueColumnSignatures = convertDataColumnsToDataSignatures(valueColumns);
+
+    // we need to convert untyped Javascript arrays to typed arrays that are compatible with .NET
+    const identityColumnsList = TypedArray.create(
+        DataColumnSignature,
+        identityColumnSignatures
+    );
+    const categoryColumnsList = TypedArray.create(
+        DataColumnSignature,
+        categoryColumnSignatures
+    );
+
+    // first convert the DataColumnSignatures to ColumnAggregations, then create a TypedArray
+    const valueColumnsAgg = valueColumnSignatures.map(col => new ColumnAggregation(col, targetMeasure));
+
+    const step1_pivot = new PivotTransformation();
+    step1_pivot.IdentityColumns = identityColumnsList;
+    step1_pivot.CategoryColumns = categoryColumnsList;
+    step1_pivot.ResultNamingExpression = STEP1_RESULTNAMINGEXPRESSION;
+    step1_pivot.ValueColumns = TypedArray.create(
+        ColumnAggregation,
+        valueColumnsAgg
+    );
+
+    dfb.AddTransformation(step1_pivot);
+    let flow = dfb.Build();
+    let reader = dfb.Execute(DataSourcePromptMode.None)
+
+//2: unpivot
+
+    // generate a list of new columns created by the pivot containing all columns except ones ending with ".0"
+    const newPivotColumns = Array.from(reader.Columns).filter(
+        col => col.Name.startsWith(titleColumns[0].Name) && !col.Name.endsWith(".0")
+    );
+
+    const step2_unpivot = new UnpivotTransformation();
+    step2_unpivot.CategoryName = "Category";
+    step2_unpivot.CategoryType = DataType.String;
+    step2_unpivot.ResultName = "Value";
+    step2_unpivot.ResultType = DataType.Real;
+    step2_unpivot.IdentityColumns = TypedArray.create(
+        DataColumnSignature,
+        identityColumnSignatures
+    );
+    step2_unpivot.ValueColumns = TypedArray.create(
+        DataColumnSignature,
+        newPivotColumns.map(col => new DataColumnSignature(col))
+    );
+
+    dfb.AddTransformation(step2_unpivot);
+    flow = dfb.Build();
+    reader = dfb.Execute(DataSourcePromptMode.None);
+
+
+
+//3: replace (Empty) with 0
+/*
+since the dfb returns a DataFlow, it's not possible to use getColumn to get the new Value column
+instead we need to get the DataType for the "Value" column in order to construct a signature
+*/
+    const valueColumn = OutParam.create(DataRowReaderColumn);
+    reader.Columns.TryGetColumn("Value", valueColumn.out)!;
+    const valueColumnSignature = new DataColumnSignature(valueColumn);
+
+    const typedValue = OutParam.create(System.Object);
+    if (!valueColumn.DataType.Formatter.TryParse("0.0", typedValue.out))
+        throw new Error("Cannot parse 0.0 to the correct type for the Value column.");
+
+//??? ReplaceValuesTransformation API doesn't allow for replacing (Empty) with a number
+    const step3_replaceValues = new ReplaceValuesTransformation(valueColumnSignature, null, typedValue);
+
+// using a ReplaceColumnTransformation with `SN()` (SubstituteNull) function as a workaround
+//const step3_replaceValues = createReplaceColumnTransform(valueColumnSignature, "Value", "Real(SN([Value], 0))");
+    dfb.AddTransformation(step3_replaceValues);
+
+//4: add Zone and Area columns
+    const step4a_addColumns = new AddCalculatedColumnTransformation("Zone", "Split([Category], '.', 1)");
+    const step4b_addColumns = new AddCalculatedColumnTransformation("Area", "Split([Category], '.', 2)");
+    dfb.AddTransformation(step4a_addColumns);
+    dfb.AddTransformation(step4b_addColumns);
+
+    flow = dfb.Build();
+    reader = dfb.Execute(DataSourcePromptMode.None);
+
+//5: pivot back
+    // get new Zone and Area columns
+    const zoneColumn = OutParam.create(DataRowReaderColumn);
+    reader.Columns.TryGetColumn("Zone", zoneColumn.out)!
+    const zoneColumnSignature = new DataColumnSignature(zoneColumn);
+    const areaColumn = OutParam.create(DataRowReaderColumn);
+    reader.Columns.TryGetColumn("Area", areaColumn.out)!;
+    const areaColumnSignature = new DataColumnSignature(areaColumn);
+
+    const step5_pivot = new PivotTransformation();
+    step5_pivot.IdentityColumns = identityColumnsList;
+    step5_pivot.ResultNamingExpression = STEP5_RESULTNAMINGEXPRESSION;
+    step5_pivot.CategoryColumns = TypedArray.create(
+        DataColumnSignature,
+        [zoneColumnSignature, areaColumnSignature]
+    );
+    step5_pivot.ValueColumns = TypedArray.create(
+        ColumnAggregation,
+        [new ColumnAggregation(valueColumnSignature, targetMeasure)]
+    );
+
+    dfb.AddTransformation(step5_pivot);
+
+    flow = dfb.Build();
+    reader = dfb.Execute(DataSourcePromptMode.None);
+
+    return flow;
+}
+
+/*
+* applies an AddColumns operation to the operationDataTable.
+*
+* @param operationDataTable - the DataTable to add columns to
+* @param sourceDataTable - the DataTable to copy columns from
+* @param pkColumns - an array of DataColumns to use as the primary key for the join
+* @param ignoredColumns - an array of DataColumns to ignore when adding columns
+*/
+function applyAddColumns(
+    operationDataTable: Spotfire.Dxp.Data.DataTable,
+    dataSource: Spotfire.Dxp.Data.DataSource,
+    operationPkColumns: Spotfire.Dxp.Data.DataColumn[],
+    sourcePkColumns: Spotfire.Dxp.Data.DataColumn[],
+    ignoredColumns: Spotfire.Dxp.Data.DataColumnSignature[],
+): void {
+    const ignoredColumnsList = TypedArray.create(DataColumnSignature, ignoredColumns);
+
+    // get the columns from the source table based on the names of the primary key columns
+    // okay to use ! here since we have already validated
+    const operationTablePkColumnSignatures = convertDataColumnsToDataSignatures(operationPkColumns);
+    const sourceTablePkColumnSignatures = convertDataColumnsToDataSignatures(sourcePkColumns);
+
+    // zip the two sets of signatures into tuple pairs
+    const columnMapItems = zip(sourceTablePkColumnSignatures, operationTablePkColumnSignatures);
+
+    // initialize the map and add primary key columns to it in pairs
+    const map = new Dictionary(DataColumnSignature, DataColumnSignature);
+    columnMapItems.forEach(([sourceCol, operationCol]) => map.Add(sourceCol, operationCol));
+
+    const settings = new AddColumnsSettings(map, JoinType.LeftOuterJoin, ignoredColumnsList);
+    operationDataTable.AddColumns(dataSource, settings);
+}
+
+export function applyWaferTransform({
+    document,
+    application,
+    baseDataTable,
+    primaryKeyColumns,
+    targetColumnTitles,
+    targetColumnValues,
+    targetMeasure,
+    outputDataTableName,
+}: ApplyWaferTransformParameters) {
+
+    // debug values
+    primaryKeyColumns = D_PRIMARYKEYCOLUMNS;
+    targetColumnTitles = D_TARGETCOLUMNTITLES;
+    targetColumnValues = D_TARGETCOLUMNVALUES;
+    targetMeasure = D_TARGETMEASURE;
+    outputDataTableName = D_OUTPUTDATATABLENAME;
+
+    const inputsValid = validateInputs();
+    if (!inputsValid) {
+        throw new Error("Invalid input parameters.")
+    }
+
+    const baseDataSource = new DataTableDataSource(baseDataTable);
+
+    // parse columns from csv
+    const pkColumns = getColumnsFromCsv(baseDataTable, primaryKeyColumns);
+    const titleColumns = getColumnsFromCsv(baseDataTable, targetColumnTitles);
+    const valueColumns = getColumnsFromCsv(baseDataTable, targetColumnValues);
+
+        // the first transformation sequence happens on the new operation data table
+    // it's okay to use ! here since we have already validated the inputs
+    const firstTitleColumn = titleColumns.shift()!;
+    const firstValueColumn = valueColumns.shift()!;
+
+    const newDataSource = applyTransformSeries(
+        application.ImportContext,
+        baseDataSource,
+        pkColumns,
+        [firstTitleColumn],
+        [firstValueColumn],
+        targetMeasure
+    )
+
+    // create a new data table with the transformations applied
+    const operationDataTable = createOrReplaceDataTable(document, outputDataTableName, newDataSource);
+
+    // apply an AddColumns operation followed by our transformation series for each remaining title column
+    for (let i = 0; i < titleColumns.length; i++) {
+
+        const titleCol = titleColumns[i];
+        const valueCol = valueColumns[i];
+
+        const transformedDataTableDataSource = applyTransformSeries(
+            application.ImportContext,
+            newDataSource,
+            pkColumns,
+            [titleCol],
+            [valueCol],
+            targetMeasure
+        );
+
+        // based on the names of the provided pk columns, look up the corresponding columns in the
+        //  source table based on their name
+        const sourcePkColumns = pkColumns.map(col => baseDataTable.Columns.Item.get(col.Name)!);
+
+        applyAddColumns(
+            operationDataTable,
+            transformedDataTableDataSource,
+            pkColumns,
+            sourcePkColumns,
+            [] // no columns to ignore
+        )
+    }
+
+
+}
+
+RegisterEntryPoint(applyWaferTransform);
