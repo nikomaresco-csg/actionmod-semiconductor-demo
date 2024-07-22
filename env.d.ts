78bcc5e5153164a5a513348e55b1cc9833dec5d6
Niko Maresco P52 <niko.maresco@cloud.com>
2024-07-22 16:43:34 -0400

    manifest changes

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,14 @@
 // This file is auto-generated and will be overwritten on build.
 /// <reference types="@spotfire/mods-api/action-mods/api.d.ts" />
 
+interface CreateWaferMapchartParameters {
+    /** The loaded {@link Spotfire.Dxp.Application.Document}. */
+    document: Spotfire.Dxp.Application.Document;
+
+    /** The current {@link Spotfire.Dxp.Application.AnalysisApplication}. */
+    application: Spotfire.Dxp.Application.AnalysisApplication;
+}
+
 interface ApplyWaferTransformParameters {
     /** The loaded {@link Spotfire.Dxp.Application.Document}. */
     document: Spotfire.Dxp.Application.Document;
@@ -10,14 +18,6 @@ interface ApplyWaferTransformParameters {
 
     /** The data table to transform */
     baseDataTable: DataTable;
-}
-
-interface CreateWaferMapchartParameters {
-    /** The loaded {@link Spotfire.Dxp.Application.Document}. */
-    document: Spotfire.Dxp.Application.Document;
-
-    /** The current {@link Spotfire.Dxp.Application.AnalysisApplication}. */
-    application: Spotfire.Dxp.Application.AnalysisApplication;
 }
 
 interface CreateLinechartParameters {
