6fb5a1e127edacb1f0d274213ada2960acc3a3bc
Niko Maresco P52 <niko.maresco@cloud.com>
2024-08-02 11:47:51 -0400

    fixed connection check logic

--- src/utils/library.ts
+++ src/utils/library.ts
@@ -49,7 +49,7 @@ export function getColorSchemeFromLibrary(
     application: Spotfire.Dxp.Application.AnalysisApplication,
     libraryPath: string
 ): Spotfire.Dxp.Framework.Library.LibraryItem {
-    if (checkServerConnection(application)) {
+    if (!checkServerConnection(application)) {
         throw new Error("Cannot access library when not connected to a server");
     }
 
@@ -79,3 +79,28 @@ export function getColorSchemeFromDocument(
     return document.ColoringTemplates.Item.get(colorSchemeName)!;
 }
 
+/*
+* searches the library with a given query string and returns the results
+* 
+* @param application - the current application context
+* @param searchExpression - the query string to search the library with
+* @returns a collection of LibraryItems that match the search query
+*/
+export function searchLibrary(
+    application: Spotfire.Dxp.Application.AnalysisApplication,
+    searchExpression: string
+): Spotfire.Dxp.Framework.Library.LibraryItemCollection {
+    if (!checkServerConnection(application)) {
+        throw new Error("Cannot access library when not connected to a server");
+    }
+
+    const libraryManager = application.GetService(Spotfire.Dxp.Framework.Library.LibraryManager);
+    if (libraryManager == null || !libraryManager) {
+        throw new Error("LibraryManager service not available");
+    }
+
+    return libraryManager.Search(
+        searchExpression,
+        Spotfire.Dxp.Framework.Library.LibraryItemRetrievalOption.IncludePath
+    );
+}
